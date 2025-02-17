e51bd3300d4658db47144ed9d038de9c85e63c4d
Felizius33715 <162054503+Felizius33715@users.noreply.github.com>
2025-02-18 00:22:04 +0700

    shape.cpp

--- shape.cpp
+++ shape.cpp
@@ -4,25 +4,135 @@
 
 class Shape {
 public:
-    static const int line = 0;
-    static const int sqr = 1;
-    static const int cube = 2;
-    static const int circle = 3;
-    static const int cylinder = 4;
+    virtual ~Shape() = default;
 
-    Shape() = default;
-    Shape(int type, const std::vector<int>& coords);
-    Shape(int type, int x, int y, double R, double H = 0);
+    virtual void shift(int dx, int dy, int dz = 0) {
+        x += dx;
+        y += dy;
+        z += dz;
+    }
 
-    int getType() const { return type; }
+    virtual void scaleX(int factor) {
+        x *= factor;
+    }
 
-    double calculateSquare() const;
-    double calculateVolume() const;
+    virtual void scaleY(int factor) {
+        y *= factor;
+    }
 
-    std::vector<int> coords; // Координаты фигуры
-    int type = line;
-    double volume = 0;
-    double square = 0;
-    double height = 0;
-    double radius = 0;
+    virtual void scaleZ(int factor) {
+        z *= factor;
+    }
+
+    virtual void scale(int factor) {
+        scaleX(factor);
+        scaleY(factor);
+        scaleZ(factor);
+    }
+
+    virtual double getSquare() const = 0;
+    virtual double getVolume() const = 0;
+
+protected:
+    int x = 0, y = 0, z = 0;
+};
+
+class Line : public Shape {
+public:
+    Line(int _x1, int _y1, int _x2, int _y2) 
+        : x1(_x1), y1(_y1), x2(_x2), y2(_y2) {}
+
+    void shift(int dx, int dy, int dz = 0) override {
+        x1 += dx; y1 += dy;
+        x2 += dx; y2 += dy;
+    }
+
+    void scaleX(int factor) override {
+        x1 *= factor; x2 *= factor;
+    }
+
+    void scaleY(int factor) override {
+        y1 *= factor; y2 *= factor;
+    }
+
+    void scaleZ(int factor) override {}
+
+    void scale(int factor) override {
+        scaleX(factor);
+        scaleY(factor);
+    }
+
+    double getSquare() const override {
+        return 0;
+    }
+
+    double getVolume() const override {
+        return 0;
+    }
+
+private:
+    int x1, y1, x2, y2;
+};
+
+class Rectangle : public Shape {
+public:
+    Rectangle(int _x, int _y, int width, int height)
+        : width(width), height(height) {
+        x = _x;
+        y = _y;
+    }
+
+    double getSquare() const override {
+        return width * height;
+    }
+
+    double getVolume() const override {
+        return 0;
+    }
+
+    void scaleX(int factor) override {
+        width *= factor;
+    }
+
+    void scaleY(int factor) override {
+        height *= factor;
+    }
+
+    void scaleZ(int factor) override {}
+
+private:
+    int width, height;
+};
+
+class Cube : public Shape {
+public:
+    Cube(int _x, int _y, int _z, int size) 
+        : size(size) {
+        x = _x;
+        y = _y;
+        z = _z;
+    }
+
+    double getSquare() const override {
+        return 6 * size * size;
+    }
+
+    double getVolume() const override {
+        return size * size * size;
+    }
+
+    void scaleX(int factor) override {
+        size *= factor;
+    }
+
+    void scaleY(int factor) override {
+        size *= factor;
+    }
+
+    void scaleZ(int factor) override {
+        size *= factor;
+    }
+
+private:
+    int size;
 };
